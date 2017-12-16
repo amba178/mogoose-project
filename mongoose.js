50e0ee8ae68a4cd3ddaa38e62d00cc8d5eb3b333
Salem Amba <salemamba@gmail.com>
2017-12-16 13:57:56 -0500

    first commit

---
+++ mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+
+//mongoose.connect(uri, [optional], [callback])
+mongoose.connect('mongodb://localhost/test')
+
+let Book = mongoose.model('Book', {name: String})
+let practicalNodeBook = new Book({name: 'Practical Node.js'})
+
+practicalNodeBook.save((err, results) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  } else {
+    console.log('Saved: ', results)
+    process.exit(0)
+  }
+})
